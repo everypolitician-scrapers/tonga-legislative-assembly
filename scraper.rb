370c6ede429a23756da6832c611b9b568f778cac
Chris Mytton <chrismytton@gmail.com>
2017-01-30 15:30:26 +0100

    Initial commit

---
+++ scraper.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'pry'
+require 'scraped'
+require 'scraperwiki'
+
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
+# require 'scraped_page_archive/open-uri'
+
+def scrape(h)
+  url, klass = h.to_a.first
+  klass.new(response: Scraped::Request.new(url: url).response)
+end
