0ec83d62518008f5cb3967e2187e05f9864cca51
Tony Bowden <tony@tmtm.com>
2018-09-21 08:41:37 +0100

    Remove NoblePage
    We don't need this any more, as nothing is different for them.

--- scraper.rb
+++ scraper.rb
@@ -7,25 +7,17 @@ require_rel 'lib'
 
 require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
-# require 'scraped_page_archive/open-uri'
 
 def scrape(h)
   url, klass = h.to_a.first
   klass.new(response: Scraped::Request.new(url: url).response)
 end
 
-ScraperWiki.sqliteexecute('DROP TABLE data') rescue nil
-
-{
-  'peoples' => MemberPage,
-  'nobles'  => NoblePage,
-}.each do |section, klass|
+data = %w[peoples nobles].flat_map do |section|
   url = "http://parliament.gov.to/members-of-parliament/#{section}/"
-
-  data = scrape(url => MembersPage).member_urls.map do |mem_url|
-    scrape(mem_url => klass).to_h
-  end
-
-  data.each { |mem| puts mem.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h } if ENV['MORPH_DEBUG']
-  ScraperWiki.save_sqlite(%i[id], data)
+  scrape(url => MembersPage).member_urls.map { |mem_url| scrape(mem_url => MemberPage).to_h }
 end
+data.each { |mem| puts mem.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h } if ENV['MORPH_DEBUG']
+
+ScraperWiki.sqliteexecute('DROP TABLE data') rescue nil
+ScraperWiki.save_sqlite(%i[id], data)
