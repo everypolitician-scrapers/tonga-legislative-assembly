1b6fac96dd008610c9f5520201984824ac3628c4
Tony Bowden <tony@tmtm.com>
2018-09-21 08:38:00 +0100

    Don't scrape phone numbers
    These add a lot of complexity and and aren't really that interesting to us
    anyway.

--- lib/member_page.rb
+++ lib/member_page.rb
@@ -36,29 +36,6 @@ class MemberPage < Scraped::HTML
     noko.css('.email').map(&:text).join(';')
   end
 
-  # This handles the following variations of the "Mobile Phone" field:
-  #
-  #   Mobile Phone : 676 7714548
-  #   Mobile Phone : 676 7755555 or 676 88855555
-  #   Mobile Phone : +676 66858
-  #   Email: fvakata11@gmail.comMobile Phone : 676 7716294Home Phone :23851
-  field :cell do
-    noko.xpath("//tr/td[contains(., 'Mobile Phone')]").text
-        .match(/Mobile Phone[[:space:]]*?\:[[:space:]]*?([^\nH]+)/)
-        .to_a[1].to_s.tidy.gsub(' or', ';')
-  end
-
-  # This handles the following variations of the "Home Phone" field:
-  #
-  #   Home Phone : 676 25773
-  #   Home Phone : +676 31194
-  #   Email: fvakata11@gmail.comMobile Phone : 676 7716294Home Phone :23851
-  field :phone do
-    noko.xpath("//tr/td[contains(., 'Home Phone')]").text
-        .match(/Home Phone[[:space:]]*?:[[:space:]]*?([\+\d[[:space:]]]+)/)
-        .to_a[1].to_s.tidy
-  end
-
   field :source do
     url
   end
--- lib/noble_page.rb
+++ lib/noble_page.rb
@@ -3,13 +3,4 @@
 require_relative 'member_page'
 
 class NoblePage < MemberPage
-  # We need to override this field because the Nobles use "Mob number" rather
-  # than "Mobile Phone" to specify the cell number. This field has a simpler
-  # implementation from the parent because the field is always in the following
-  # format:
-  #
-  #   Mob number : 8403558
-  field :cell do
-    noko.xpath("//ul/li[contains(., 'Mob number :')]").text.split(':', 2).last.to_s.tidy
-  end
 end
